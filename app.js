40563eb51047ed197dff6b5695258c41b0d66be7
Anna Drewing <drewing@subshell.com>
2023-01-04 15:41:54 +0100

    button almost styled

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let userData = {
 };
 
 const tiles = document.querySelectorAll(".tile");
+console.log(tiles);
 const steps = document.querySelectorAll(".step");
 
 for (const [index, step] of steps.entries()) {
@@ -18,11 +19,18 @@ for (const [index, step] of steps.entries()) {
   });
 }
 
+for (const [index, tile] of tiles.entries()) {
+  if (index > 0) {
+    tile.classList.remove("tile");
+    tile.classList.add("vanishTile");
+  }
+}
+
 function showTile(tileNr) {
   tiles.forEach((tile) => {
-    tile.classList.remove("vanishTile");
-    tile.classList.add("tile");
+    tile.classList.add("vanishTile");
+    tile.classList.remove("tile");
   });
-  tiles[tileNr].classList.remove("tile");
-  tiles[tileNr].classList.add("vanishTile");
+  tiles[tileNr].classList.remove("vanishTile");
+  tiles[tileNr].classList.add("tile");
 }
