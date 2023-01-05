49f5815552315402beaa5e0d8b35c91339a610d5
Anna Drewing <drewing@subshell.com>
2023-01-05 12:29:45 +0100

    step1 form user input saved in an object

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const tiles = document.querySelectorAll(".tile");
+console.log(tiles);
+const steps = document.querySelectorAll(".step");
 let userData = {
   name: "e.g. Stephen King",
   email: "e.g. stephenking@lorem.com",
@@ -9,9 +12,55 @@ let userData = {
   customizableProfile: "no",
 };
 
-const tiles = document.querySelectorAll(".tile");
-console.log(tiles);
-const steps = document.querySelectorAll(".step");
+applyPlaceholderUserData();
+
+function applyPlaceholderUserData() {
+  let name = document.getElementById("name");
+  name.addEventListener("click", () => {
+    clearField(name);
+  });
+  name.addEventListener("change", () => {
+    applyUserInput(name);
+    console.log("New user data:" + userData.name);
+    console.log(userData);
+  });
+
+  let email = document.getElementById("email");
+  email.addEventListener("click", () => {
+    clearField(email);
+  });
+  email.addEventListener("change", () => {
+    applyUserInput(email);
+    console.log("New user data:" + userData.email);
+    console.log(userData);
+  });
+  let phoneNr = document.getElementById("phoneNr");
+  phoneNr.addEventListener("click", () => {
+    clearField(data);
+  });
+  phoneNr.addEventListener("change", () => {
+    applyUserInput(data);
+    console.log("New user data:" + userData.phoneNr);
+    console.log(userData);
+  });
+
+  let dataList = [name, email, phoneNr];
+  for (data of dataList) {
+    data.value = userData[data.name];
+    data.classList.add("placeholderText");
+  }
+}
+
+function clearField(inputField) {
+  if (inputField.classList.contains("placeholderText")) {
+    inputField.classList.remove("placeholderText");
+    inputField.value = "";
+  }
+}
+
+function applyUserInput(inputField) {
+  userData[inputField.name] = inputField.value;
+}
 
 for (const [index, step] of steps.entries()) {
   step.addEventListener("click", () => {
